0a85dbc3dfc14a10307d8fbdb1412c29b710bc0d
veeru8899 <veerendra.nulu@gmail.com>
2022-01-17 23:55:17 +0530

    spring boot basic structure

---
+++ src/main/java/com/veerendra/SpringbootCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.veerendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudApiApplication.class, args);
+	}
+
+}
